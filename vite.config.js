b2ced76c671a2051cc3f5ae221e8923029ac863c
brojamalu <digital.jamalu@gmail.com>
2026-01-13 23:23:35 +0700

    Update config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/spk-isp-batam/',
+  build: {
+    outDir: 'dist',
+    // Copy file .nojekyll ke dist
+    copyPublicDir: true
+  },
+  publicDir: 'public'
 })
